01d84f56ad5debd12ba52a601af54a6a9894927d
alicia chaing <aliciawchiang@gmail.com>
2022-07-19 10:13:21 -0700

    camera

--- camera/ViewController.swift
+++ camera/ViewController.swift
@@ -10,19 +10,12 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageDisplay: UIImageView!
-    
     var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
         // Do any additional setup after loading the view.
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
-            imageDisplay.image = selectedImage
-        }
     }
 
     @IBAction func takeSelfie(_ sender: Any) {
@@ -32,6 +25,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBAction func choosePhotoTapped(_ sender: Any) {
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageDisplay.image = selectedImage
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
     }
     
 }
